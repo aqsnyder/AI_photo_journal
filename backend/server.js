8724b0306f202029e1d13721eb379deaed98f7da
Snyder <aaron.q.snyder@gmail.com>
2024-09-07 18:26:15 -0400

    server.js admin service account for firebase fixed

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const admin = require('firebase-admin'); // Import Firebase Admin SDK
 admin.initializeApp({
     credential: admin.credential.cert({
         "type": "service_account",
-        "project_id": process.env.FIREBASE_PROJECT_ID,
+        "project_id": process.env.FIREBASE_PROJECT_ID,  // Ensure this is pulling from the .env file
         "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
         "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         "client_email": process.env.FIREBASE_CLIENT_EMAIL,
